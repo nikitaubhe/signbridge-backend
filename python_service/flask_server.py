dfa6ee9b1d8f611b5efcfe1cac8160e4e28290b7
Nikita Ubhe <nikitaubhe0204@gmail.com>
2026-04-09 14:08:15 +0530

    fix app startup

--- python_service/flask_server.py
+++ python_service/flask_server.py
@@ -12,7 +12,18 @@ from PIL import Image
 from collections import deque
 import logging
 
+from flask import Flask
+import os
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Backend is running 🚀"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 app = Flask(__name__)
 CORS(app)  
