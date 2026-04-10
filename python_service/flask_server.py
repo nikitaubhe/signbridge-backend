acd21bcc5a418583d9c90e7bd10b409ef99449c0
Nikita Ubhe <nikitaubhe0204@gmail.com>
2026-04-10 15:39:53 +0530

    flaskserverfix5

--- python_service/flask_server.py
+++ python_service/flask_server.py
@@ -219,8 +219,13 @@ word_map = {
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["https://signbridge-frontend-fcxbl6nbl-nikitaubhes-projects.vercel.app"])
-
+CORS(app) 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 def build_model():
     m = Sequential()
